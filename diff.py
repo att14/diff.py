2f4de0b3add5926034a063c959e424e55e6ca900
Andrew Tribone <atribone@gmail.com>
2013-02-05 00:39:24 -0800

    changed StringDiffer slightly and added functionality for TupleDiffer

--- diff.py
+++ diff.py
@@ -1,7 +1,29 @@
+from __future__ import division
 from difflib import SequenceMatcher
+
+from lib import levenshtein
+
+start = '<'
+end = '>'
+
+
+class DifferRegistrar(type):
+
+    type_to_differ_cls = {}
+
+    def __init__(cls, *args):
+        cls.type_to_differ_cls[cls.differ_type] = cls
+
+    @classmethod
+    def get_differ(cls, old, new):
+        return cls.type_to_differ_cls[type(old)](old, new)
 
 
 class Base(object):
+
+    __metaclass__ = DifferRegistrar
+
+    differ_type = None
 
     def __init__(self, old, new, color=False):
         self.old, self.new = old, new
@@ -13,37 +35,46 @@ class Base(object):
 
 class NoneDiffer(Base):
 
+    differ_type = type(None)
+
     @property
     def output(self):
         if self.new is None:
             return str(None)
-        return '<' + str(None) + '>'
+        return start + str(None) + end
 
 
 class BooleanDiffer(Base):
+
+    differ_type = bool
 
     @property
     def output(self):
         if self.old is self.new:
             return str(self.old)
-        return '<' + str(self.old) + '>'
+        return start + str(self.old) + end
 
 
 class IntegerDiffer(Base):
+
+    differ_type = int
 
     def __init__(self, old, new):
         super(IntegerDiffer, self).__init__(str(old), str(new))
 
     @property
     def output(self):
-        return StringDiffer(self.old, self.new).output
+        return StringDiffer(self.old, self.new).output.strip('"')
 
 
 class FloatDiffer(IntegerDiffer):
-    pass
+
+    differ_type = float
 
 
 class LongDiffer(IntegerDiffer):
+
+    differ_type = long
 
     def __init__(self, old, new):
         super(LongDiffer, self).__init__(old, new)
@@ -53,26 +84,29 @@ class LongDiffer(IntegerDiffer):
         return super(LongDiffer, self).output + 'L'
 
 
-class ComplexDiffer(Base):
-    pass
+class ComplexDiffer(IntegerDiffer):
+
+    differ_type = complex
 
 
 class StringDiffer(Base):
 
-    highlight_start = '<'
-    highlight_end = '>'
+    differ_type = str
 
     def __init__(self, old, new):
         super(StringDiffer, self).__init__(old, new)
         self.highlighted = [None for _ in range(len(self.old))]
         self.matches = sorted(SequenceMatcher(a=self.old,
-                                              b=self.new).get_matching_blocks(),
-                              key=lambda m: m.a)
+                                              b=self.new).get_matching_blocks()[:-1],
+                              key=lambda m: m.size,
+                              reverse=True)
 
     @property
     def output(self):
+        if (levenshtein(self.old, self.new) / len(self.old)) > 0.9:
+            return '"<' + self.old + '>"'
+
         for match in self.matches:
-            # XXX: Need more testing around this statement.
             if match.size == 1 and match.a != match.b:
                 continue
 
@@ -82,41 +116,59 @@ class StringDiffer(Base):
         result = []
         for char, highlight in zip(list(self.old), self.highlighted):
             if highlight is None:
-                result.append('<' + char + '>')
+                result.append(start + char + end)
             else:
                 result.append(char)
 
-        return ''.join(result).replace('><', '')
+        return '"' + ''.join(result).replace('><', '') + '"'
 
 
 class UnicodeDiffer(Base):
-    pass
+
+    differ_type = unicode
 
 
 class TupleDiffer(Base):
-    pass
+
+    differ_type = tuple
+
+    @property
+    def output(self):
+        differs = []
+        for old_item, new_item in zip(self.old, self.new):
+            if old_item != new_item:
+                differs.append(DifferRegistrar.get_differ(old_item, new_item))
+
+        result = '('
+        for differ in differs:
+            result += differ.output + ', '
+        return result.strip(', ') + ')'
 
 
 class ListDiffer(Base):
-    pass
+
+    differ_type = list
 
 
 class SetDiffer(Base):
-    pass
+
+    differ_type = set
 
 
 class DictDiffer(Base):
-    pass
+
+    differ_type = dict
 
 
 class FunctionDiffer(Base):
-    pass
+
+    differ_type = type(lambda x: None)
 
 
-class LambdaDiffer(Base):
-
-    # XXX: This does not belong here. Just keeping it for refrence.
-    # def isalambda(l):
-    #   return isinstance(l, type(lambda: None)) and l.__name__ == '<lambda>'
-
-    pass
+#class LambdaDiffer(Base):
+#
+#    # XXX: This does not belong here. Just keeping it for refrence.
+#    # def isalambda(l):
+#    #   return isinstance(l, type(lambda: None)) and l.__name__ == '<lambda>'
+#
+#    pass
--- diff_test.py
+++ diff_test.py
@@ -30,7 +30,11 @@ class IntegerDifferAcceptanceTestCase(unittest.TestCase):
         integer_diff = diff.IntegerDiffer(1234567890, 1234557890)
         self.assertEqual('12345<6>7890', integer_diff.output)
 
-    def test_integer_diff_beginning(self):
+    def test_integer_diff_one_char(self):
+        integer_diff = diff.IntegerDiffer(1234567890, 1024)
+        self.assertEqual('1<23>4<567890>', integer_diff.output)
+
+    def test_integer_same(self):
         integer_diff = diff.IntegerDiffer(1234567890, 1234567890)
         self.assertEqual('1234567890', integer_diff.output)
 
@@ -49,6 +53,17 @@ class FloatDifferAcceptanceTestCase(unittest.TestCase):
         self.assertEqual('3.<1>4529', float_diff.output)
 
 
+class ComplexDifferAcceptanceTestCase(unittest.TestCase):
+
+    def test_complex_diff(self):
+        complex_diff = diff.ComplexDiffer(12j, 2j)
+        self.assertEqual('<1>2j', complex_diff.output)
+
+    def test_complex_addition(self):
+        complex_diff = diff.ComplexDiffer(2+1j, 2j)
+        self.assertEqual('<(2+1j)>', complex_diff.output)
+
+
 class LongDifferAcceptanceTestCase(unittest.TestCase):
 
     def test_long_diff(self):
@@ -62,70 +77,103 @@ class StringAcceptanceTestCase(unittest.TestCase):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is the new string')
 
-        self.assertEqual('this is the <old> string', string_diff.output)
+        self.assertEqual('"this is the <old> string"', string_diff.output)
+
+    def test_xxx(self):
+        string_diff = diff.StringDiffer('bane is the string',
+                                        'dean is the string')
+
+        self.assertEqual('"<b>an<e> is the string"', string_diff.output)
+
+
+    def test_one_char(self):
+        string_diff = diff.StringDiffer('abcdefg', 'hialmn')
+        self.assertEqual('"<abcdefg>"', string_diff.output)
+
+        string_diff = diff.StringDiffer('abcdefghilm', 'hialmn')
+        self.assertEqual('"<abcdefg>hilm"', string_diff.output)
 
     def test_string_diff_beginning(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'kiss is the old string')
 
-        self.assertEqual('<thi>s is the old string', string_diff.output)
+        self.assertEqual('"<thi>s is the old string"', string_diff.output)
 
     def test_string_diff_end(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is the old strina')
 
-        self.assertEqual('this is the old strin<g>', string_diff.output)
+        self.assertEqual('"this is the old strin<g>"', string_diff.output)
 
     def test_string_same(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is the old string')
 
-        self.assertEqual('this is the old string', string_diff.output)
+        self.assertEqual('"this is the old string"', string_diff.output)
 
     def test_string_diff_len(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is the old')
 
-        self.assertEqual('this is the old< string>', string_diff.output)
+        self.assertEqual('"this is the old< string>"', string_diff.output)
 
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is the old strings')
 
-        self.assertEqual('this is the old string', string_diff.output)
+        self.assertEqual('"this is the old string"', string_diff.output)
 
     def test_string_newline(self):
         string_diff = diff.StringDiffer('this is \nthe old string',
                                         'this is the old string')
 
-        self.assertEqual('this is <\n>the old string', string_diff.output)
+        self.assertEqual('"this is <\n>the old string"', string_diff.output)
 
     def test_string_multiple_diff(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this old the is string')
 
-        self.assertEqual('this <is> the <old> string', string_diff.output)
+        self.assertEqual('"this <is> the <old> string"', string_diff.output)
 
         string_diff = diff.StringDiffer('this is the old string',
                                         'this new the is string')
 
-        self.assertEqual('this <is> the <old> string', string_diff.output)
+        self.assertEqual('"this <is> the <old> string"', string_diff.output)
 
     def test_string_long_diff(self):
         string_diff = diff.StringDiffer('this is the old string',
                                         'this is abcdefg old string')
 
-        self.assertEqual('this is <the> old string', string_diff.output)
+        self.assertEqual('"this is <the> old string"', string_diff.output)
 
     def test_string_empty(self):
         string_diff = diff.StringDiffer('this is the old string', '')
-        self.assertEqual('<this is the old string>', string_diff.output)
+        self.assertEqual('"<this is the old string>"', string_diff.output)
 
     def test_string_contains_highlight_markers(self):
         string_diff = diff.StringDiffer('this is >< old string',
                                         'this is the old string')
 
-        self.assertEqual('this is <><> old string', string_diff.output)
+        self.assertEqual('"this is <><> old string"', string_diff.output)
 
+    def test_string_levenshtein(self):
+        string1 = 'ZrBZyHZGUqkAqFuTnyfrvxVZbhFLkFExNCoqJLNhXLqqBuSpvMCKbKndacWJVwQaTxinMreutIqrcfAGvRrRllFowDiIsLQPNmMK'
+        string2 = 'GTiZfTDkJUXhQmydpOVfkuqGGqWYTkRsVwJJsEejETSxSLuebcQvEgFlDiYzMqajSkjANBISPnRmZgbfLqPhRNnzJTUpYCcfqTMy'
+
+        string_diff = diff.StringDiffer(string1, string2)
+
+        self.assertEqual('"<%s>"' % string1, string_diff.output)
+
+    def test_string_one_char_diff_end(self):
+        # XXX: Should this highlight the c at the end?
+        string_diff = diff.StringDiffer('abkxyeyec', 'abxyec')
+        self.assertEqual('"ab<k>xye<yec>"', string_diff.output)
+
+
+class TupleDifferAcceptanceTestCase(unittest.TestCase):
+
+    def test_tuple_diff(self):
+        tuple_diff = diff.TupleDiffer((12345, 'abcdefg'), (2345, 'abcefg'))
+        self.assertEqual('(<1>2345, "abc<d>efg")', tuple_diff.output)
 
 
 if __name__ == '__main__':
